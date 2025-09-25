23dedd83d33c3bfd94892060d77a9bb85aeeb134
Olaf_dev <kenzaumezaki@gmail.com>
2025-09-25 16:27:22 +0900

    feat: create ERC20InterFace

--- ERC20.sol
+++ ERC20.sol
@@ -15,7 +15,7 @@ contract ERC20Interface {
     event Transfer(address indexed from, address indexed to, uint tokens);
     event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
 }
-
+        
 // ----------------------------------------------------------------------------
 // Safe Math Library
 // ----------------------------------------------------------------------------
